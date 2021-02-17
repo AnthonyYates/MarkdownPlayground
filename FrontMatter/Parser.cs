7924ee9e97b4e82b94a7b203ed266bbbb0032754
Tony Yates <tony@anthonyyates.com>
2021-02-17 15:06:52 +0100

    Add project files.

---
+++ FrontMatter/Parser.cs
@@ -0,0 +1,55 @@
+﻿using Markdig;
+using Markdig.Extensions.Yaml;
+using Markdig.Syntax;
+using System;
+using System.Linq;
+using YamlDotNet.Serialization;
+
+namespace FrontMatterParser
+{
+    /// <summary>
+    /// Parses markdown file and returns front matter as strongly typed class.
+    /// </summary>
+    /// <remarks>Idea from https://khalidabuhakmeh.com/parse-markdown-front-matter-with-csharp </remarks>
+    public static class Parser
+    {
+        private static readonly IDeserializer YamlDeserializer;
+        private static readonly MarkdownPipeline Pipeline;
+
+        static Parser()
+        {
+            YamlDeserializer = new DeserializerBuilder()
+                .IgnoreUnmatchedProperties()
+                .Build();
+
+            Pipeline = new MarkdownPipelineBuilder()
+            .UseYamlFrontMatter()
+            .Build();
+        }
+
+        public static T Parse<T>(this string markdown)
+        {
+            var document = Markdown.Parse(markdown, Pipeline);
+            var block = document
+                .Descendants<YamlFrontMatterBlock>()
+                .FirstOrDefault();
+
+            if (block == null)
+                return default;
+
+            var yaml =
+                block
+                // yep...we have to call .Lines 2x
+                .Lines // StringLineGroup[]
+                .Lines // StringLine[]
+                .OrderByDescending(x => x.Line)
+                .Select(x => $"{x}\n")
+                .ToList()
+                .Select(x => x.Replace("---", string.Empty))
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .Aggregate((s, agg) => agg + s);
+
+            return YamlDeserializer.Deserialize<T>(yaml);
+        }
+    }
+}
---
+++ MarkdownPlayground/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using YamlDotNet.Serialization;
+
+namespace MarkdownPlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var markdownString =
+@"---
+title: Article title
+so.date: 12.12.2020
+so.envir: [onsite, online]
+so.product: api-core, api-services, win, web, netserver, mobile
+so.area: [configuration, administration, end-user, security, osql, entities, rows, connectors, mirroring, SOAP, REST]
+so.category: [contact, person, sale, selection, ticket, oauth, openid connect]
+so.topic: concept, how-to, reference, tutorial
+---
+
+Here is a brief introduction.
+
+## Sub-heading
+
+Here is some content
+
+### Sub-sub-heading
+
+Here is some sub sub content.
+";
+            var fm = FrontMatterParser.Parser.Parse<SoFrontMatter>(markdownString);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+        }
+    }
+
+
+    public class SoFrontMatter
+    {
+        [YamlMember(Alias = "so.area")]
+        public string[] Area { get; set; }
+
+        [YamlMember(Alias = "so.category")]
+        public string[] Category { get; set; }
+        
+        [YamlMember(Alias = "so.envir")]
+        public string[] Environment { get; set; }
+        
+        [YamlMember(Alias = "so.product")]
+        public string Product { get; set; }
+
+        [YamlMember(Alias = "tags")]
+        public string Tags { get; set; }
+
+        [YamlMember(Alias = "title")]
+        public string Title { get; set; }
+
+        [YamlMember(Alias = "so.date")]
+        public DateTime WrittenDate { get; set; }
+
+        [YamlIgnore]
+        public IList<string> GetTags => Tags?
+            .Split(",", StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => x.Trim())
+            .ToArray();
+    }
+}
